ca5781fad690a17f17ac4f7a991455e940861202
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2024-01-22 23:22:25 +0500

    Your commit message

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,4 @@
-// import React from 'react';
-// import { Provider } from 'react-redux';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import reportWebVitals from './reportWebVitals';
-// import App from './App';
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import IndividualCurrency from './pages/IndividualCurrency';
-// import Store from './app/Store'
-// import Last from './pages/home/Last';
 
-
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <App /> },
-//   {
-//     path: "/IndividualCurrency",
-//     element: <IndividualCurrency />
-//   },
-//   {
-//     path: "/Aud",
-//     element: <Last/>
-//   },
-
-// ]);
-
-
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <Provider store={Store}>
-//     <React.StrictMode>
-//      <App/>
-//     </React.StrictMode>
-//   </Provider>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 import React from 'react';
 import { Provider } from 'react-redux';
